e43a465b2d8044633524ffd9430ce73c78dc10a9
cleofe <chuamanihu@unsa.edu.pe>
2020-10-07 22:41:52 -0500

    Agregando boton de regreso

--- app/src/main/java/com/example/saludo/RecibiendoSaludo.java
+++ app/src/main/java/com/example/saludo/RecibiendoSaludo.java
@@ -2,7 +2,9 @@ package com.example.saludo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class RecibiendoSaludo extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class RecibiendoSaludo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recibiendo_saludo);
+
+    }
+    public void Regresar(View view){
+        Intent i=new Intent(this, MainActivity.class);
+
+        startActivity(i);
     }
 }
